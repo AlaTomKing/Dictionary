c50c4577dd9431f5aeceaec3e3449a3898412983
Amogus Bread <89712796+AlaTomKing@users.noreply.github.com>
2022-03-05 16:26:22 +0100

    change

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-console.log("something")
-
 const wrapper = document.querySelector(".wrapper")
 const searchInput = wrapper.querySelector("input")
+const searchButton = wrapper.querySelector("button")
+
+function search() {
+    if (searchInput.value) {
+        console.log(searchInput.value)
+    } else {
+        console.log("Bruh, did you even put anything? You can't put nothing and expect something out of it.")
+    }
+}
 
 searchInput.addEventListener("keyup", input => {
-    if (input.key === "Enter" && input.target.value) {
-        console.log(input.target.value)
+    if (input.key === "Enter") {
+        search()
     }
+})
+
+searchButton.addEventListener("click", () => {
+    search()
 })
